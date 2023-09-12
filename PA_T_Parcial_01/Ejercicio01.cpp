f8f9f02b035f690b46867717b055c05da269c440
rayr <rayrdevp@gmail.com>
2023-09-12 08:15:59 -0600

    Dedicated to javascript, the best lenguage of my home.

--- PA_T_Parcial_01/Ejercicio01.cpp
+++ PA_T_Parcial_01/Ejercicio01.cpp
@@ -1,4 +1,7 @@
 #include "Ejercicio01.h"
+#include <string>
+
+using namespace std;
 
 char Ejercicio01::lower(char c)
 {
@@ -10,5 +13,24 @@ char Ejercicio01::lower(char c)
 
 bool Ejercicio01::isPalindrome(const char* phrase, int length)
 {
-    return false;
+    
+    string cleanPhrase;
+    for (int i = 0; i < length; i++) {
+        char c = lower(phrase[i]);
+        if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+            cleanPhrase += c;
+        }
+    }
+    
+    int n = cleanPhrase.size();
+    for (int i = 0; i < n / 2; i++) {
+        if (cleanPhrase[i] != cleanPhrase[n - i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
 }
+
+
+
--- PA_T_Parcial_01/Ejercicio02.cpp
+++ PA_T_Parcial_01/Ejercicio02.cpp
@@ -1,6 +1,27 @@
 #include "Ejercicio02.h"
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+
+
+using namespace std;
 
 int Ejercicio02::rob(int houses[], int n)
 {
-    return 0;
+    if (n == 0) return 0;
+    if (n == 1) return houses[0];
+
+    int* dp = (int*)malloc(sizeof(int) * n);
+
+    dp[0] = houses[0];
+    dp[1] = max(houses[0], houses[1]);
+
+    for (int i = 2; i < n; i++) {
+        dp[i] = max(dp[i - 1], dp[i - 2] + houses[i]);
+    }
+
+    return dp[n - 1];
+	
 }
+
+
